39f794061bc183d11d1903f8deb2838e7e8f3c7a
Sara Vieira <sara.vieira@mindera.com>
2017-07-16 17:40:45 +0100

    add bin

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/* global require process */
+var exec = require('child_process').exec
+
+var cwd = process.cwd ().replace (/\\/g, '/')
+var suffix = '/node_modules/preact-cli-plugin-flow'
+
+if (cwd.endsWith (suffix)) {
+    var root = cwd.substr (0, cwd.length - suffix.length)
+
+    exec('npm run setup', {cwd: root})
+}
