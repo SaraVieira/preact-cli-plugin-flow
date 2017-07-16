cb939fd3c34321397ec2a5fa438ab3095917231d
Sara Vieira <sara.vieira@mindera.com>
2017-07-16 18:05:25 +0100

    fix package.json

--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,24 @@
 #!/usr/bin/env node
 
-console.log('running')
-
 /* global require process */
 var exec = require('child_process').exec
 
 var cwd = process.cwd().replace (/\\/g, '/')
 var suffix = '/node_modules/preact-cli-plugin-flow'
 
-console.log(cwd.endsWith(suffix), cwd)
 
 if (cwd.endsWith(suffix)) {
     var root = cwd.substr(0, cwd.length - suffix.length)
+    exec('flow init', {cwd: root}, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`)
+            return
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+    })
 
-    console.log(root)
-
-    exec('npm run setup', {cwd: root}, (error, stdout, stderr) => {
+    exec('flow-typed update', {cwd: root}, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`)
             return
