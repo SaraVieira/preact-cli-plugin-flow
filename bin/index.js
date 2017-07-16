d6fec7ca81cbf4fdd9b100841081c116c0f0a6b6
Sara Vieira <sara.vieira@mindera.com>
2017-07-16 18:15:10 +0100

    fix package.json

--- bin/index.js
+++ bin/index.js
@@ -6,28 +6,27 @@ var exec = require('child_process').exec
 var cwd = process.cwd().replace (/\\/g, '/')
 var suffix = '/node_modules/preact-cli-plugin-flow'
 
-const installFlowBin = () =>
-    exec('npm install --save-dev flow-bin', {cwd: root}, (error) => {
-        if (error) {
-            console.error(`exec error: ${error}`) // eslint-disable-line no-console
-            return
-        }
-
-
-        console.log('Flow Bin installed')  // eslint-disable-line no-console
-
-        flowTypedUpdate()
-    })
-
-const flowTypedUpdate = () =>
+function flowTypedUpdate() {
     exec('flow-typed update', {cwd: root}, (error) => {
         if (error) {
             console.error(`exec error: ${error}`) // eslint-disable-line no-console
             return
         }
-
         console.log('Flow-typed updated')  // eslint-disable-line no-console
     })
+}
+
+function installFlowBin() {
+    exec('npm install --save-dev flow-bin', {cwd: root}, (error) => {
+        if (error) {
+            console.error(`exec error: ${error}`) // eslint-disable-line no-console
+            return
+        }
+        console.log('Flow Bin installed')  // eslint-disable-line no-console
+
+        flowTypedUpdate()
+    })
+}
 
 if (cwd.endsWith(suffix)) {
     var root = cwd.substr(0, cwd.length - suffix.length)
@@ -36,7 +35,6 @@ if (cwd.endsWith(suffix)) {
             console.error(`exec error: ${error}`) // eslint-disable-line no-console
             return
         }
-
         console.log('Flow initialized')  // eslint-disable-line no-console
 
         installFlowBin()
