f159e7c679191aade0f23ebb6b5672b21c07e908
Sara Vieira <sara.vieira@mindera.com>
2017-07-16 17:54:09 +0100

    add bin

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
+console.log('running')
+
 /* global require process */
 var exec = require('child_process').exec
 
 var cwd = process.cwd().replace (/\\/g, '/')
 var suffix = '/node_modules/preact-cli-plugin-flow'
 
-console.log(cwd.endsWith(suffix))
+console.log(cwd.endsWith(suffix), cwd)
 
 if (cwd.endsWith(suffix)) {
     var root = cwd.substr(0, cwd.length - suffix.length)
