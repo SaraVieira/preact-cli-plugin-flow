53f380ea8f2243d4917e8d23ff0e93e436b227b6
Sara Vieira <hey@iamsaravieira.com>
2017-08-30 16:47:58 +0100

    lets try again

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ const exec = require('child_process').exec
 const ora = require('ora')
 const path = require('path')
 const fs = require('fs')
+const spawn = require('cross-spawn-promise')
 
 const cwd = process.cwd().replace(/\\/g, '/')
 const suffix = '/node_modules/preact-cli-plugin-flow'
@@ -13,22 +14,47 @@ const spinner = ora('Loading unicorns').start()
 const srcPath = path.join(cwd, '.flowconfig')
 const dstPath = path.join(root, '.flowconfig')
 const flowConfig = fs.readFileSync(srcPath)
-const {
-    bin, // (String)  path to executed binary (yarn or npm)
-    isYarn, // (Boolean) if it was executed with Yarn
-    isNPM // (Boolean) if it was executed with npm
-} = require('nyr')
+const yarnLock = path.join(root, 'yarn.lock')
+const { isYarn } = require('nyr')
 
-function installFlowBin() {
-    console.log(bin)
-    spinner.start('Installing Dependencies')
-    exec(`${bin} flow-bin --save-dev`, { cwd: root }, error => {
-        if (error) {
-            spinner.fail(`Install Error: ${error}`)
-            return
+const install = async (yarn, cwd, packages, env) => {
+    let isDev = env === 'dev' ? true : false
+    let isYarnAvailable = fs.existsSync(yarnLock) || isYarn
+    let toInstall = packages.filter(Boolean)
+
+    // pass null to use yarn only if yarn.lock is present
+    if (!yarn) {
+        try {
+            let stat = await fs.stat(path.resolve(cwd, 'yarn.lock'))
+            yarn = stat.isFile()
+        } catch (e) {
+            yarn = false
         }
-        spinner.succeed('Dependencies installed')
-    })
+    }
+
+    if (isYarnAvailable && yarn) {
+        let args = ['add']
+        if (isDev) {
+            args.push('-D')
+        }
+
+        return await spawn('yarn', [...args, ...toInstall], {
+            cwd,
+            stdio: 'ignore'
+        })
+    }
+
+    await spawn(
+        'npm',
+        ['install', isDev ? '--save-dev' : '--save', ...toInstall],
+        { cwd, stdio: 'ignore' }
+    )
+}
+
+const installFlowBin = async () => {
+    spinner.start('Installing Dependencies')
+    await install(isYarn, root, ['flow-bin'], 'dev')
+    spinner.succeed('Dependencies installed')
 }
 
 spinner.start('Flow is scaffolding')
